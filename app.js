fe36f21c863220a7d02680c63bb303422add4635
Samuel Glenn <contact@canyo.net>
2026-08-28 01:39:22 -0700

    Cache-bust app.js, tune reveal observer threshold for tall sections

--- app.js
+++ app.js
@@ -79,7 +79,7 @@
         io.unobserve(entry.target);
       }
     });
-  }, { threshold: 0.12, rootMargin: '0px 0px -40px 0px' });
+  }, { threshold: 0.15, rootMargin: '0px 0px -60px 0px' });
   els.forEach(function (el) { io.observe(el); });
 })();
 
